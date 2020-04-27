193b4078bee995a08fca8752e1d7ca7911f5bf5b
Faizan <faizanahmad9619@gmail.com>
2020-04-27 01:17:48 -0400

    Added arduino file

---
+++ Circulating_lights/Circulating_lights.ino
@@ -0,0 +1,20 @@
+enum arduinoStates {
+  stateSelect,  // Turns on LED 0 on pin D12
+  stateOne,     // Turns on LED 1 on pin D11
+  stateTwo      // Turns on LED 2 on pin D10
+  stateThree    // Turns on LED 3 on pin D9
+  stateFour,    // Turns on LED 4 on pin D8
+  stateFive,    // Turns on LED 5 on pin D7
+  stateSix,     // Turns on LED 6 on pin D6
+  stateSeven    // Turns on LED 7 on pin D5
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
